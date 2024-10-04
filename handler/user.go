6094dd1ad1166ac0c6787459d430fabc76478de3
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2024-10-04 11:13:12 +0700

    feat: make user input validation

--- handler/user.go
+++ handler/user.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"be-bwastartup/helper"
 	"be-bwastartup/user"
 	"net/http"
 
@@ -18,19 +19,34 @@ func NewUserHandler(userService user.Service) *userHandler {
 func (h *userHandler) RegisterUser(c *gin.Context) {
 	// tangkap input dari user
 	var input user.RegisterUserInput
+
+
 	err := c.ShouldBindJSON(&input)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, nil)
+			// validation in here
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		errorReponse := helper.APIResponse("Register account failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusBadRequest, errorReponse)
 		return
 	}
 
-	user, nil := h.userService.RegisterUser(input)
-	if(err != nil){
-		c.JSON(http.StatusBadRequest, nil)
+	newUser, nil := h.userService.RegisterUser(input)
+	if err != nil {
+		errorResponse := helper.APIResponse("Register account failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, errorResponse)
 		return
 	}
 
-	c.JSON(http.StatusOK, user)
+	userToken := "token" // ini sementara
+
+
+	registerResponse := user.FormatUser(newUser, userToken)
+
+	response := helper.APIResponse("Account has been registered", http.StatusCreated, "success", registerResponse)
+
+	c.JSON(http.StatusOK, response)
 	// map input dari user ke struct RegisterUserInput
 	// struct di atas kita passing sebagai parameter service
 	// di file user.go
---
+++ helper/helper.go
@@ -0,0 +1,48 @@
+package helper
+
+import (
+	"github.com/go-playground/validator/v10"
+)
+
+// Define the response structure
+type response struct {
+	Meta meta `json:"meta"`
+	Data interface{} `json:"data"`
+}
+
+// Define the meta structure
+type meta struct {
+	Message string `json:"message"`
+	Code    int    `json:"code"` // This should be a single int representing an HTTP status code
+	Status  string `json:"status"`
+}
+
+// APIResponse function returns a response object
+func APIResponse(message string, code int, status string, data interface{}) response {
+
+	// Construct the meta part of the response
+	meta := meta{
+		Message: message,
+		Code:    code,
+		Status:  status,
+	}
+
+	// Construct and return the full response
+	jsonResponse := response{
+		Meta: meta,
+		Data: data,
+	}
+
+	return jsonResponse
+}
+
+
+func FormatValidationError(err error) []string {
+	var errors []string;
+
+	for _, e := range err.(validator.ValidationErrors) {
+		errors = append(errors, e.Error())
+	}
+
+	return errors
+}
