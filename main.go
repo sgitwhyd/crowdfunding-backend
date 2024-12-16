d4c292ba131a1e6de58b7e9fd02dc7c26845923e
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2024-12-16 19:42:40 +0700

    feat(cors): add additional allowed origin for CORS configuration

--- main.go
+++ main.go
@@ -84,7 +84,7 @@ func main(){
 	router := gin.Default()
 	// enable cors
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "*"}, // Adjust to your frontend URLs
+		AllowOrigins:     []string{"http://localhost:3000", "*", "http://funding.api.sigitwahyu.tech"}, // Adjust to your frontend URLs
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
